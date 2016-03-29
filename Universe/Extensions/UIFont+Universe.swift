4b36b4f2ccad8266421aba5b5c56c968d87db0d7
wangzhenyujiang <wangzhenyujiang@163.com>
2016-03-29 14:57:14 +0800

    29 afternoon company

---
+++ Universe/Extensions/UIFont+Universe.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+Universe.swift
+//  Universe
+//
+//  Created by mazingwang on 16/3/29.
+//  Copyright © 2016年 王振宇. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    static func timeLabelFont() -> UIFont {
+        return UIFont(name: "HelveticaNeue", size: 35)!
+    }
+}
--- Universe/ViewControllers/Start/StartViewController.swift
+++ Universe/ViewControllers/Start/StartViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class StartViewController: BaseViewController {
+    @IBOutlet weak var timerLabel: MZTimerLabel!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        setupTimerLabel()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+    }
 }
 
 //MARK: IBAction
@@ -18,5 +28,28 @@ extension StartViewController {
     
     @IBAction func backAction(sender: AnyObject) {
         navigationController?.popViewControllerAnimated(true)
+    }
+    
+    @IBAction func timeButtonAction(sender: AnyObject) {
+        timerLabel.start()
+    }
+}
+
+extension StartViewController: MZTimerLabelDelegate {
+    
+    func timerLabel(timerLabel: MZTimerLabel!, finshedCountDownTimerWithTime countTime: NSTimeInterval) {
+        
+    }
+    
+    func timerLabel(timerLabel: MZTimerLabel!, countingTo time: NSTimeInterval, timertype timerType: MZTimerLabelType) {
+        
+    }
+    
+    private func setupTimerLabel() {
+        timerLabel.timeFormat = "mm:ss"
+        timerLabel.timerType = MZTimerLabelTypeTimer
+        timerLabel.setCountDownTime(1800)
+        timerLabel.textColor = UIColor.whiteColor()
+//        timerLabel.font = UIFont.timeLabelFont()
     }
 }
