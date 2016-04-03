8766bea429f988527cc071e6059d6bd89bb2ad17
mazingyu <mazingyu@gmail.com>
2016-04-03 12:53:15 +0800

    3 noon commit

---
+++ Universe/Views/GoldAlterView/GoldAlterView.swift
@@ -0,0 +1,13 @@
+//
+//  GoldAlterView.swift
+//  Universe
+//
+//  Created by 王振宇 on 16/4/3.
+//  Copyright © 2016年 王振宇. All rights reserved.
+//
+
+import UIKit
+
+class GoldAlterView: UIView {
+
+}
